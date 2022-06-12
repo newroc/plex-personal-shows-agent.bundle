67320b8bc804cc489849a75e443492f9b0f54750
newroc <newroc@gmail.com>
2022-06-12 21:05:05 +0800

    Update __init__.py
    修改了更新海报的逻辑，之前是只有找到meta.json时才更新海报，修改为，如果没有meta.json，则仍然更新海报，优先在当前目录下找cover.jpg文件，如果没有则到上一级目录下找cover.jpg文件。

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -7,29 +7,33 @@ class PersonalShowsAgent(Agent.TV_Shows):
     name, primary_provider, fallback_agent, contributes_to, accepts_from, languages, persist_stored_files = 'NewrocPersonal Shows', True, None,['com.plexapp.agents.thetvdb'], ['com.plexapp.agents.localmedia'], [Locale.Language.NoLanguage],False
 
     def search(self, results, media, lang):
+        Log.Info('进入search()')
         filename = String.Unquote(media.filename)
         infoData = LoadAnyJSON(os.path.splitext(filename)[0] + '.info.json')
+        Log.Info('加载: %s 从: %s' % (infoData['title'], os.path.splitext(filename)[0] + '.info.json'))
         if infoData:
             ytdl_id = 'movie-' + infoData['extractor'] + '-' + infoData['id']
             metadata = MetadataSearchResult(
                 id=ytdl_id,
-                name=media.name,
+                name=infoData['title'],
                 year=int(infoData['upload_date'][:4]) if 'upload_date' in infoData else None,
                 score=infoData['average_rating'] if 'average_rating' in infoData else 100,
                 lang=lang
             )
             results.Append(metadata)
+            Log.Info('metadata=: %s ' % (metadata))
         
         #Log.Info('Searching Metadata')
         #x = "My Title %s %s %s" % (media.name, media.episode, media.season)
         #results.Append(MetadataSearchResult(id = media.filename, score = 100, name=media.filename, lang=Locale.Language.NoLanguage))
 
     def update_season(self, season_id, summary):
+        Log.Info(' 进入update_season()')
         ip_address = Prefs['ip_address']
         port = Prefs['port']
         username = Prefs['username']
         password = Prefs['password']
-
+        
         if not ip_address or not port or not username or not password:
             Log.Info('Missing Preferences, Skipping Summary Update')
             return
@@ -39,13 +43,13 @@ class PersonalShowsAgent(Agent.TV_Shows):
 
         metadata = json.loads(HTTP.Request(url=('http://%s/library/metadata/%s' %(host, season_id)), immediate=True, headers={'Accept': 'application/json'}).content)
         section_id = metadata['MediaContainer']['librarySectionID']
-
+        Log.Info('HAHA_season_id='+season_id+";summary="+summary+";metadata:"+metadata)
         request = HTTP.Request(url=('http://%s/library/sections/%s/all?summary.value=%s&type=3&id=%s' % (host, section_id, urllib.quote(summary), season_id)), method='PUT' )
         request.load()
 
     def update_poster(self, metadata, link, base_path = None):
         try:
-            Log.Info('Updating poster link: %s base: %s' % (link, base_path))
+            Log.Info('更新海报链接: %s base: %s' % (link, base_path))
             if not link or not metadata:
                 Log.Info('Skipping poster update. Link or metadata missing')
                 return
@@ -65,7 +69,7 @@ class PersonalShowsAgent(Agent.TV_Shows):
                 if not os.path.exists(poster_path):
                     poster_path = os.path.normpath(os.path.join(base_path, '../', link))
 
-            Log.Info('Poster path %s' % (poster_path))
+            Log.Info('海报路径 %s' % (poster_path))
             data = Core.storage.load(poster_path)
             media_hash = hashlib.md5(data).hexdigest()
             metadata.posters[media_hash] = Proxy.Media(data)
@@ -73,21 +77,19 @@ class PersonalShowsAgent(Agent.TV_Shows):
             Log.Error('Error updating poster %s' % e.message)
 
     def update(self, metadata, media, lang):
-        Log.Info('Updating Metadata8')
-        Log.Info(media.seasons[media.seasons.keys()[0]])
+        Log.Info('进入update()')
         tempSeason=media.seasons[media.seasons.keys()[0]];
         tempEpisodes=tempSeason.episodes[tempSeason.episodes.keys()[0]]
         main_path = tempEpisodes.items[0].parts[0].file
         filename = String.Unquote(main_path)
         dir_name = os.path.normpath(os.path.join(filename, '../'))
         show_name=os.path.basename(dir_name)
-        Log.Info('show_name='+show_name)
+        Log.Info('更新节目信息='+show_name)
         #  main_path = media.seasons['1'].episodes['1'].items[0].parts[0].file
 
         show_path = os.path.normpath(os.path.join(main_path, '../'))
         meta_path = os.path.join(show_path, 'meta.json')
         Log.Info('meta_path='+meta_path)
-        Log.Info(metadata)
         metadata.title = show_name
 
         #  if not os.path.exists(meta_path):
@@ -100,9 +102,11 @@ class PersonalShowsAgent(Agent.TV_Shows):
 
         if os.path.exists(meta_path):
             meta_json = json.loads(Core.storage.load(meta_path))
-            Log.Info(meta_json)
-            metadata.summary = meta_json.get('summary', '')
-            metadata.studio = meta_json.get('publisher', '')
+            # Log.Info(meta_json)
+            trySet(metadata, 'summary', any(meta_json, ['summary', 'description']))
+            trySet(metadata, 'studio', any(meta_json, ['publisher','uploader']))
+            # metadata.summary = meta_json.get('summary', '')
+            # metadata.studio = meta_json.get('publisher', '')
             metadata.genres.clear()
             for genre in meta_json.get('tags', []):
                 metadata.genres.add(genre)
@@ -113,9 +117,13 @@ class PersonalShowsAgent(Agent.TV_Shows):
                 role.role = actor.get('role', '')
                 role.name = actor.get('name', '')
                 role.photo = actor.get('photo', '')
-
+        
             clear_posters(metadata)
             self.update_poster(metadata, meta_json.get('show_thumbnail', 'cover.jpg'), show_path)
+
+        else:
+            clear_posters(metadata)
+            self.update_poster(metadata, 'cover.jpg', show_path)
 
         for season_index in media.seasons.keys():
             season_metadata = metadata.seasons[season_index]
@@ -125,6 +133,7 @@ class PersonalShowsAgent(Agent.TV_Shows):
             season_name = os.path.basename(season_path)
 
             season_summary = season_name
+            Log.Info('更新季信息='+season_name)
 
             clear_posters(season_metadata)
             #  if meta_json and 'seasons' in meta_json and season_index in meta_json['seasons']:
@@ -152,7 +161,7 @@ class PersonalShowsAgent(Agent.TV_Shows):
                 ApplyInfoToMetadata(episode_info_data,episode_metadata)
                 #episode_metadata.title = episode_info_data.get("title")
                 #episode_metadata.summary=episode_info_data.get("description")
-                Log.Info(episode_metadata)
+                Log.Info("更新影片信息："+episode_metadata.title)
 
 def LoadAnyJSON(directory, filenames=[]):
     result = False
